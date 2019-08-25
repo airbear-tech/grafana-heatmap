b3279735651abb2392aa9ef65ee25a6c6289c317
Stefan Haselwimmer <sh801@cam.ac.uk>
2019-08-25 18:04:40 +0100

    All files

---
+++ dist/module.js
@@ -0,0 +1,14 @@
+System.register(["./heatmap_ctrl"], function (_export, _context) {
+  "use strict";
+
+  var HeatMapCtrl;
+  return {
+    setters: [function (_heatmap_ctrl) {
+      HeatMapCtrl = _heatmap_ctrl.HeatMapCtrl;
+    }],
+    execute: function () {
+      _export("PanelCtrl", HeatMapCtrl);
+    }
+  };
+});
+//# sourceMappingURL=module.js.map
